0dd74a11a1c432efde22b1b3f66580124c3e7807
Armand Adroher Salvia <aadroher@gmail.com>
2016-06-20 19:09:12 +0100

    Adds comments to Ping Pong Table.

--- pingpong/models/ping_pong_tables.py
+++ pingpong/models/ping_pong_tables.py
@@ -3,11 +3,17 @@ TABLES = ('A', 'B')
 
 
 def get_ping_pong_tables():
-
+    """
+    :return: Una lista con instancias de PingPongTable que
+      corresponden a los nombres definidos en TABLES.
+    """
     return [PingPongTable(name) for name in TABLES]
 
 
 class PingPongTable:
+    """
+    Representa una mesa de ping pong.
+    """
 
     def __init__(self, name):
         self.name = name
